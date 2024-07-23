3ab00f3d064a4d6f9c2e165150bc37d6c0379dd2
addiayoub <addiayoub2005@gmail.com>
2024-07-23 15:38:23 +0100

    new LandingPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import LESAVIS from './Avis client/LESAVIS'
+import Footer from './Footer/Footer'
+import Hover from './icons/Hover'
+import Navbar from './Navbar/Navbar'
+import Slider from './slider/Slider'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+import Animation from './Animation/Animation'
+
+
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+  <Animation/>
+  <Navbar/>
+  <Slider/>
+  <Hover/>
+  <LESAVIS/>
+  <Footer/>
+    </div>
   )
 }
 
